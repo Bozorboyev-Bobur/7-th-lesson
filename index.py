dac38b17ef5649cebf363d988ffdb4dcc471b01b
Bobur Bazarbayev <bozorboyevbobur2004@gmail.com>
2021-12-11 15:30:15 +0500

    first commit

---
+++ index.py
@@ -0,0 +1,41 @@
+# name1 = 'Zafar'
+# name2 = 'Abdulkhakim'
+
+# names = ['Zafar', 'Abdulkhakim', 'Bobur', 'Zakir', 'Shukhrat', 'Aziz', 'Otabek', 'Amalmalik']
+# ages = [21, 15 , 17 , 20, 21]
+# names.reverse()
+# names =names[::-1]
+# print()
+# print(names[0:7:2])
+
+# names.append('elbek')
+# names.append(23)
+# names.append(True)
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(5))
+# names.insert(0, 'Elbek')
+# print(len(names))
+# names.remove('Bobur')
+# ages.remove(21)
+# ages.remove(29)
+# print(ages)
+
+
+# hrs = int(input('Enter hours:'))
+# mins = int(input('Enter minutes:'))
+# delta = int(input('Enter delta hours:'))
+
+# if hrs >= 0 and 0 <= mins <=59:
+#     new_hrs = (hrs + delta) % 24
+#     if new_hrs <= 9: new_hrs = '0' + str(new_hrs)
+#     if mins <= 9: mins = '0' + str(mins)
+#     print(f'{new_hrs}:{mins}')
+# else: print('Error')
+
+# b = 1
+
+# a = 9 if b > 2 else 10
+
+# print(a)
